0701c9f23fb9b01f624f87a80c1ad9037d1c8fa4
Eden R <eden@whoplusyou.com>
2019-10-28 13:33:01 -0400

    Pig Latin translation function with examples

---
+++ pig_latin_translator.py
@@ -0,0 +1,23 @@
+def pigLatinTranslator(word):
+    vowels = "aeiouAEIOU"
+    word = str(word)
+    if word.isdigit():
+        print ("Please submit a word.")
+    elif len(word) < 1:
+        print ("Please submit a longer word.")
+    else:
+        if word[0] in vowels:
+            return word + "-yay"
+        for letter in word:
+            word = word[1:] + word[0]
+            if word[0] in vowels:
+                return word + "-ay"
+        return word[1:] + word[0] + "-ay" 
+
+
+wordsToTranslate = ['example', 'school', 'question', 'fig',  'answer', 'computer', 'eliminate', 'shred', 'why']
+
+for englishWord in wordsToTranslate:
+    pigLatinWord = pigLatinTranslator(englishWord)
+    print("\"{}\" becomes \"{}\"".format(englishWord.capitalize(), pigLatinWord))
+
